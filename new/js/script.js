e4d15a807723b9b579693ff99165f4268ee7be0e
Vlad <vladkostirya@gmail.com>
2022-06-16 22:02:30 +0300

    first commit

---
+++ new/js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function(){
+ 
+	console.log('Готов!');
+});
+/* $(document).ready(function(){
+    $('.slider').slick({
+     arrows:false,
+     dots: true,
+     infinite: true,
+     speed: 500,
+     fade: false,
+     cssEase: 'linear',
+     autoplay:true,
+     autoplaySpeed:4000, 
+
+     
+    });
+
+  });
+   */
+  /* $('.fade').slick({
+    dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+}); */
+
+/* количество отабражаемых слайдеров
+slidesToShow:3, */
+/*  кол-во пролистывания
+slidesToScroll: 3, */
+/* speed: 1000,
+easing:'liner',
+/* pauseOnFocus: true,
+pauseOnHover: true,
+pauseOnDotsHover: true, */
+/*  fade:true, */
+/* draggable:false,  */
